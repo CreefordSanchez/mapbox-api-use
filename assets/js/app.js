f9912e80c67876b5de638fa178a94a87f7a91658
Creeford Sanchez <creewrk@gmail.com>
2024-12-11 14:16:34 -0800

    fixed-problems

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+import {print} from './test.js';
+
+console.log(print);
+
+import mapboxgl from './mapbox-gl';
+
+//mapboxgl.accessToken = 'pk.eyJ1IjoiZ3VybGlua2F1ciIsImEiOiJjbHExYjM4cHUwNzE3MnBud25qNDlmc2VjIn0.Jeu9BD0h1vILAwXce8dQqw';
---
+++ assets/js/test.js
@@ -0,0 +1,1 @@
+export let print = 'hello';
