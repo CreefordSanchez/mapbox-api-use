2e1cade9c754c5e9af27aabcb694ab044364c60d
Creeford Sanchez <creewrk@gmail.com>
2024-12-11 21:01:38 -0800

    added-track-btn

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,17 +4,43 @@ import { select, listen } from "./data/utility.js";
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ3VybGlua2F1ciIsImEiOiJjbHExYjM4cHUwNzE3MnBud25qNDlmc2VjIn0.Jeu9BD0h1vILAwXce8dQqw';
 
+const trackBtn = select('.track-btn');
 const showMap = select('.map');
+const accuracy = { enableHighAccuracy: true };
 
-const map = new mapboxgl.Map({
-  container: showMap,
-  style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-74.5, 40],
-  zoom: 9
+listen(window, 'load', () => {
+  mapMarker(-74.5, 40);
 });
 
-map.on('load', () => {
+listen(trackBtn, 'click', () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      getLocation, errorHandler, accuracy
+    );
+  } else {
+    console.log('Your browser does not support geolocation.');
+  }
+});
+
+function mapMarker(longitude, latitude) {
+  const map = new mapboxgl.Map({
+    container: showMap,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [longitude, latitude],
+    zoom: 15
+  });
+
   new mapboxgl.Marker()
-    .setLngLat([-74.5, 40])
+    .setLngLat([longitude, latitude])
     .addTo(map);
-});
+}
+
+
+function getLocation(possition) {
+  let { latitude, longitude } = possition.coords;
+  mapMarker(longitude, latitude);
+}
+
+function errorHandler() {
+  console.log('Unable to find your location');
+}
